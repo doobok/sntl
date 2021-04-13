9a6ebc7fb4785a2558e3c7d44577b0eb49925711
yura <yura.chernobay@gmail.com>
2021-04-13 14:22:09 +0300

    Another slider

--- resources/js/components/MpSlideshow.vue
+++ resources/js/components/MpSlideshow.vue
@@ -1,18 +1,12 @@
 <template>
-    <carousel
-        :perPage="1"
-        :autoplayTimeout="3000"
-        :paginationEnabled="false"
-        loop
-        autoplay
-        >
+    <agile
+      autoplay
+    >
 
-      <slide v-for="slide in slides" :key="slide.id" class="pt-16 bg-cover lg:h-100 w-100"
-
-
-      :style="'background-image:url(/storage/' + slide.image + ');'"
+      <div v-for="slide in slides" :key="slide.id" class="slide pt-16 bg-cover h-full w-100 flex items-center"
+      :style="'background-image:url(\'/storage/' + slide.image + '\');'"
       >
-        <div class="container flex flex-col py-32 px-3 mx-auto justify-center items-center">
+        <div class="container flex flex-col py-12 lg:py-32 px-3 mx-auto justify-center items-center">
             <div class="my-4 p-2 text-4xl text-center lg:text-5xl font-bold leading-tight bg-gray-700 bg-opacity-50">
               <h1 v-if="slide.order == 1">{{slide.title}}</h1>
               <h2 v-else>{{slide.title}}</h2>
@@ -33,15 +27,24 @@
               </template>
             </p>
         </div>
-      </slide>
-    </carousel>
+      </div>
+      <template class="flex" slot="prevButton">
+        <svg xmlns="http://www.w3.org/2000/svg" class="hidden md:block" viewBox="0 0 24 24" fill="#fff"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>
+      </template>
+      <template class="flex" slot="nextButton">
+        <svg xmlns="http://www.w3.org/2000/svg" class="hidden md:block" viewBox="0 0 24 24" fill="#fff"><path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z"/></svg>
+      </template>
+
+    </agile>
 </template>
 
 <script>
-import { Carousel, Slide } from 'vue-carousel';
+import { VueAgile } from 'vue-agile'
 export default {
   props: ['lang', 'slides'],
-  components: { Carousel, Slide },
+  components: {
+        agile: VueAgile
+  },
   methods: {
     getUrl (url) {
       let link = null;
@@ -60,3 +63,76 @@ export default {
 }
 
 </script>
+
+<style>
+
+.agile__nav-button {
+  background: transparent;
+  border: none;
+  color: #fff;
+  cursor: pointer;
+  font-size: 24px;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  transition-duration: .3s;
+  width: 40px;
+}
+  .agile__nav-button:hover {
+    background: rgba(0, 0, 0, 0.5);
+    opacity: 1;
+  }
+
+
+  .agile__nav-button--prev {
+    left: 0;
+  }
+
+  .agile__nav-button--next {
+    right: 0;
+  }
+
+.agile__dots {
+  bottom: 10px;
+  left: 50%;
+  position: absolute;
+  transform: translateX(-50%);
+}
+
+
+.agile__dot {
+  margin: 0 10px;
+
+}
+
+.agile__dot  button {
+    background-color: transparent;
+    border: 1px solid #fff;
+    border-radius: 50%;
+    cursor: pointer;
+    display: block;
+    height: 10px;
+    font-size: 0;
+    line-height: 0;
+    margin: 0;
+    padding: 0;
+    transition-duration: .3s;
+    width: 10px;
+  }
+
+
+  .agile__dot--current,
+  .agile__dot--current:hover button {
+      background-color: #fff;
+  }
+
+
+// Slides styles
+.slide {
+  display: block;
+  height: 800px;
+  object-fit: cover;
+  width: 100%;
+}
+
+</style>
